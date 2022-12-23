bd431431c216d03cd9fd289efd1bb1749bbd0a7e
SURU SRI MUKHESH <108520098+SriMukheshSuru@users.noreply.github.com>
2022-12-23 21:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ document.querySelector(".img1").setAttribute("src",randomImageSource1);
 document.querySelector(".img2").setAttribute("src",randomImageSource2);
 
 
-if(randomNumber1>randomNumber2){document.querySelector("h1").textContent=play1 +" Wins 🚩";}
-else if (randomNumber1<randomNumber2){document.querySelector("h1").textContent=play2 +" Wins 🚩 ";}
+if(randomNumber1>randomNumber2){document.querySelector("h1").textContent="Player 1 Wins 🚩";}
+else if (randomNumber1<randomNumber2){document.querySelector("h1").textContent="Player 2 Wins 🚩 ";}
 else {document.querySelector("h1").textContent="Draw";}
